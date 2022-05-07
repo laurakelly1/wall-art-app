ca238c0d65fc46baeafa890eca177343f3f53beb
laurakelly1 <lkelly.laura@gmail.com>
2022-05-07 13:45:51 -0400

    WIP: Returning search data of first 10 artworks including title, artist name, classification, and year.

--- js/script.js
+++ js/script.js
@@ -3,15 +3,18 @@
 // https://www.artic.edu/iiif/2/1adf2696-8489-499b-cad2-821d7fde4b33/full/843,/0/default.jpg
 
 // VARIABLES
-const dataURL = "https://api.artic.edu/api/v1/artworks/129884";
+const dataURL = "https://api.artic.edu/api/v1/artworks/";
+const search = "search?q=";
+const publicDomain = "&query[term][is_public_domain]=true";
 
 // ELEMENTS
 const $title = $(".title"); // 'title'
-const $artist = $(".artist"); // 'artist_title'
+const $artist = $(".artist"); // 'artist_titles'
 const $type = $(".type"); // "classification_titles"
 const $year = $(".year"); // 'date_start"
+const $artImg = $(".artImg");
 
-const $form = $('form');
+const $form = $("form");
 const $input = $('input[type="text"]');
 
 // EVENT LISTENERS
@@ -21,19 +24,26 @@ $form.on("submit", handleGetData);
 // FUNCTIONS
 
 function handleGetData(event) {
-    event.preventDefault();
-    
-  $.ajax(dataURL).then(
-    function (art) {
-        // The art object has an object called data inside it where all the info is kept.
-        $title.text(art.data.title);
-        $artist.text(art.data.artist_title);
-        $type.text(art.data.classification_titles);
-        $year.text(art.data.date_start);
-  },
+  event.preventDefault();
+  const userInput = $input.val();
+  $.ajax(dataURL + search + userInput + publicDomain).then(
+    function (artSearch) {
+      // The art object has an object called data inside it where all the info is kept.
+      // data is an array of objects
+      artSearch.data.forEach(function (artItem) {
+        $.ajax(dataURL + artItem.id).then(function (artPiece) {
+          $("side").append(`<p class="title">${artPiece.data.title}<br></p>`);
+          $("side").append(`<p class="title">${artPiece.data.artist_titles}<br></p>`);
+          $("side").append(`<p class="title">${artPiece.data.classification_titles}<br></p>`);
+          $("side").append(`<p class="title">${artPiece.data.date_start}<br><br></p>`);
+        });
+      });
+      // in above, I want to search for painting.
+    },
+
     function (error) {
-        console.log("there is an error");
-        console.log(error);
+      console.log("there is an error");
+      console.log(error);
     }
   );
 }
