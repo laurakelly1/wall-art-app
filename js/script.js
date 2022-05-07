aa7d49e5fa5039f619d1d923da311e13bc541a09
laurakelly1 <lkelly.laura@gmail.com>
2022-05-07 14:36:14 -0400

    WIP: Able to show the artwork image underneath the image data

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ const dataURL = "https://api.artic.edu/api/v1/artworks/";
 const search = "search?q=";
 const publicDomain = "&query[term][is_public_domain]=true";
 
+const imageURL = "https://www.artic.edu/iiif/2/";
+const imageSize = "/full/843,/0/default.jpg";
+
 // ELEMENTS
 const $title = $(".title"); // 'title'
 const $artist = $(".artist"); // 'artist_titles'
@@ -26,6 +29,7 @@ $form.on("submit", handleGetData);
 function handleGetData(event) {
   event.preventDefault();
   const userInput = $input.val();
+  console.log(dataURL + search + userInput + publicDomain)
   $.ajax(dataURL + search + userInput + publicDomain).then(
     function (artSearch) {
       // The art object has an object called data inside it where all the info is kept.
@@ -33,9 +37,21 @@ function handleGetData(event) {
       artSearch.data.forEach(function (artItem) {
         $.ajax(dataURL + artItem.id).then(function (artPiece) {
           $("side").append(`<p class="title">${artPiece.data.title}<br></p>`);
-          $("side").append(`<p class="title">${artPiece.data.artist_titles}<br></p>`);
-          $("side").append(`<p class="title">${artPiece.data.classification_titles}<br></p>`);
-          $("side").append(`<p class="title">${artPiece.data.date_start}<br><br></p>`);
+          $("side").append(
+            `<p class="artist">${artPiece.data.artist_titles}<br></p>`
+          );
+          $("side").append(
+            `<p class="type">${artPiece.data.classification_titles}<br></p>`
+          );
+          $("side").append(
+            `<p class="year">${artPiece.data.date_start}<br><br></p>`
+          );
+console.log(artPiece.data.image_id)
+          $("side").append(
+            `<img src="${
+              imageURL + artPiece.data.image_id + imageSize
+            }"class="art"></img>`
+          );
         });
       });
       // in above, I want to search for painting.
