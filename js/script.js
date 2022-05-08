b5913513e0c84101e1e5d4db29c45b9e1a564c43
laurakelly1 <lkelly.laura@gmail.com>
2022-05-08 11:42:01 -0400

    WIP: Layout of page in progress, side bar aligned text with images.

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const search = "search?q=";
 const publicDomain = "&query[term][is_public_domain]=true";
 
 const imageURL = "https://www.artic.edu/iiif/2/";
-const imageSize = "/full/843,/0/default.jpg";
+const imageSize = "/full/100,/0/default.jpg";
 
 // ELEMENTS
 const $title = $(".title"); // 'title'
@@ -36,25 +36,26 @@ function handleGetData(event) {
       // data is an array of objects
       artSearch.data.forEach(function (artItem) {
         $.ajax(dataURL + artItem.id).then(function (artPiece) {
-          $("side").append(`<p class="title">${artPiece.data.title}<br></p>`);
-          $("side").append(
-            `<p class="artist">${artPiece.data.artist_titles}<br></p>`
+          $(".text").append(`<p class="title">${artPiece.data.title}<br></p>`);
+          $(".text").append(
+            `<p class="artist">Artist: ${artPiece.data.artist_titles}<br></p>`
           );
-          $("side").append(
-            `<p class="type">${artPiece.data.classification_titles}<br></p>`
+          $(".text").append(
+            `<p class="type">Type: ${artPiece.data.classification_titles}<br></p>`
           );
-          $("side").append(
-            `<p class="year">${artPiece.data.date_start}<br><br></p>`
+          $(".text").append(
+            `<p class="year">Year: ${artPiece.data.date_start}<br><br></p>`
           );
 console.log(artPiece.data.image_id)
-          $("side").append(
+          $(".image").append(
             `<img src="${
               imageURL + artPiece.data.image_id + imageSize
-            }"class="art"></img>`
+            }"class="artImage"></img>`
           );
+          // write code for if artwork has the same title, don't show
+          // Write code for if there is not data for each item, don't display the line at all. returns null.
         });
       });
-      // in above, I want to search for painting.
     },
 
     function (error) {
